22d20c979004ad703884961c823e47ed97d27c9e
zmm <zhangmingmiao>
2019-07-13 21:30:49 +0800

    132

--- vue_app_00/src/router.js
+++ vue_app_00/src/router.js
@@ -7,6 +7,16 @@ import Test from "./components/test/test.vue"
 import Test1 from "./components/test/test1.vue"
 import Test3 from "./components/test/test3.vue"
 import Import from "./components/product/import.vue"
+import Love from "./components/product/love.vue"
+import Birthady from "./components/product/birthday.vue"
+import Extend from "./components/product/extend.vue"
+import Blessing from "./components/product/blessing.vue"
+import Condole from "./components/product/condole.vue"
+import Apologize from "./components/product/apologize.vue"
+import Life from "./components/product/life.vue"
+import Opening from "./components/product/opening.vue"
+import Forever from "./components/product/forever.vue"
+import Gift from "./components/product/gift.vue"
 Vue.use(Router)
 export default new Router({
   routes: [
@@ -15,5 +25,15 @@ export default new Router({
     {path:'/Test1',component:Test1},
     {path:'/Test3',component:Test3},
     {path:'/Import',component:Import},
+    {path:'/Love',component:Love},
+    {path:'/Birthday',component:Birthady},
+    {path:'/Extend',component:Extend},
+    {path:'/Blessing',component:Blessing},
+    {path:'/Condole',component:Condole},
+    {path:'/Apologize',component:Apologize},
+    {path:'/Life',component:Life},
+    {path:'/Opening',component:Opening},
+    {path:'/Forever',component:Forever},
+    {path:'/Gift',component:Gift},
   ]
 })
