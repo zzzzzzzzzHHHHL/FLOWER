3bf80595b6cbd4ef10dee839fc087b8deaaaf9af
zmm <zhangmingmiao>
2019-07-13 18:32:03 +0800

    333

--- vue_app_00/src/router.js
+++ vue_app_00/src/router.js
@@ -5,11 +5,15 @@ import Router from 'vue-router'
 import HelloContainer from "./components/HelloWorld.vue"
 import Test from "./components/test/test.vue"
 import Test1 from "./components/test/test1.vue"
+import Test3 from "./components/test/test3.vue"
+import Import from "./components/product/import.vue"
 Vue.use(Router)
 export default new Router({
   routes: [
     {path:'/',component:HelloContainer},
     {path:'/Test',component:Test},
     {path:'/Test1',component:Test1},
+    {path:'/Test3',component:Test3},
+    {path:'/Import',component:Import},
   ]
 })
