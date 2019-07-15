a869da7fc51c95b11e02ed4866c112147024f592
zzzzzzzzzHHHHL <624585868@qq.com>
2019-07-15 21:18:48 +0800

    后台页面

--- vue_app_00/src/components/admin/admin.vue
+++ vue_app_00/src/components/admin/admin.vue
@@ -10,8 +10,21 @@
           </div>
           <div>
             <p>欢迎回来</p>
-            <h5>超级管理员</h5>
+            <a href="javascript:;">超级管理员</a>
           </div>
+        </div>
+        <div class="indexPage">
+            <i class="el-icon-s-home"></i>
+            <a href="javascript:;">后台首页</a>
+        </div>
+        <div class="manage-msg">
+          <ul>
+            <li><a href="javascript:;">商品列表</a></li>
+            <li><a href="javascript:;">添加商品</a></li>
+            <li><a href="javascript:;">添加商品</a></li>
+            <li><a href="javascript:;">添加商品</a></li>
+            <li><a href="javascript:;">添加商品</a></li>
+          </ul>
         </div>
       </div>
       <div class="right"></div>
@@ -28,21 +41,25 @@ export default {
 }
 </script>
 <style>
-  *{margin:0;padding:0;}
+  *{margin:0;padding:0;box-sizing: border-box;}
+  a{
+    text-decoration: none;
+    color:#fff;
+  }
   .wrap{
     width:100%;
     height:100vh;
     display: flex;
     justify-content: space-between;
-    box-sizing: border-box;
   }
   .left{
-    width: 15%;
+    width: 13%;
     height:100%;
     background-color: #2a3f54;
+    padding:20px;
   }
   .right{
-    width:85%;
+    width:87%;
     height:100%;
     background-color: #ddd;
   }
@@ -51,7 +68,7 @@ export default {
     font-size:24px;
     color:#fff;
     margin-top:15px;
-    text-align: center;
+    text-align: left;
   }
   .header>span{
     margin-left:5px;
@@ -65,8 +82,13 @@ export default {
   .admin-msg>div{
     float: left;
   }
+  .admin-msg::after{
+    content:"";
+    display: block;
+    clear: both;
+  }
   .admin-msg>div:first-child{
-    margin-left: 70px;
+    cursor: pointer;
   }
   .admin-msg>div:last-child{
     margin-left: 10px;
@@ -74,6 +96,46 @@ export default {
   .admin-msg>div:last-child p{
     font-size:14px;
     color:#BAB8B8;
+    font-style: italic;
+  }
+  .admin-msg>div:last-child a:hover{
+    color:red;
+  }
+  .indexPage{
+    width:100%;
+    text-align: left;
+    margin-top: 20px;
+    color:#fff;
+    font-size: 22px;
+    
+  }
+  .indexPage a{
+    margin-left:7px;
+  }
+
+  .manage-msg{
+    width:100%;
+    margin-top:30px;
+  }
+  .manage-msg ul{
+    list-style: none;
+    width:100%;
+    margin-left:0px;
+  }
+  .manage-msg ul li{
+    width:100%;
+    text-align: center;
+    margin-top:5px;
+    letter-spacing: 2px;
+    cursor: pointer;
+    background-color: #ccc;
+  }
+  .manage-msg ul li:hover{
+    background-color:#0ff;
+  }
+  .manage-msg ul li a{
+    color:#333;
+    font-weight: bold;
   }
 </style>
 
